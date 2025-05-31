b82fa6b48bee33b440f894a776e0d4c332ffa59f
fernald <57603654+fernald@users.noreply.github.com>
2025-05-31 11:04:06 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
 import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage } from '@toolpad/core/SignInPage';
+
 import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+
 
 
 
